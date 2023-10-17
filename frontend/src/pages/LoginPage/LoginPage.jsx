9c0393ddbbf2b8fcab4c45a2dd3d889c3db5f2b0
Mariano Torres <marianxtorres@gmail.com>
2023-10-17 00:50:12 -0300

    loginPage

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,8 @@
 import { useNavigate } from 'react-router-dom';
-import style from './LoginPage.css'
+import './LoginPage.css'
 import { useState } from 'react';
 import axios from 'axios'
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 
 // Pagina para iniciar sesion
 const LoginPage = () => {
@@ -29,40 +29,39 @@ const LoginPage = () => {
                 console.log(response.data);
             })
 
-        setUser({ 
+        setUser({
             email: '',
             password: ''
         })
     }
 
     return (
-
-
-        
-        <div className={style.form}>
-         
-            <form onSubmit={login} className={style.form}>
-            
-            <h1>Inicio de sesion</h1> 
-               <div className='input1'>
-                <input placeholder='Email' type="text" name="email" onChange={onChangeHandler} value={user.email} /> 
-                </div>
-                <div className='input2'>
-                <input placeholder='Password' type="password" name="password" onChange={onChangeHandler} value={user.password} />
-                </div>
-               <div className='boton'>
-                <button className={style.button} type="submit">Sign In</button>
-                </div>
-
-                <p>Don't have an account yet?</p>
-
-                <Link to='/register'><button className={style.button} type='button'>Sign Up</button></Link>
-                      
-            </form>
+        <div className='containerLogin'>
+            <div className='messageLogin'>
+                <h1>¡Bienvenido de nuevo a Grafica Angel!</h1>
+                <p>Regístrate y Personaliza tus Propios Productos Sublimables</p>
             </div>
-  
-            
-    ) 
+            <div className='containerFormLogin'>
+                <Link to='/' className='backPrincipalPage'>Inicio</Link>
+                <div className='containerTitleForm'>
+                    <p>Inicio de Sesion</p>
+                </div>
+                <form onSubmit={login} className='formLogin'>
+
+                    <input placeholder='Email' type="text" name="email" onChange={onChangeHandler} value={user.email} />
+
+                    <input placeholder='Contraseña' type="password" name="password" onChange={onChangeHandler} value={user.password} />
+
+                    <button className='buttonLogin' type="submit">Iniciar Sesion</button>
+
+                    <p className='noAccount'>¿Aún no tienes una cuenta?</p>
+
+                    <Link to='/register'><button className='buttonLogin' type='button'>Registrarse</button></Link>
+
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default LoginPage 
